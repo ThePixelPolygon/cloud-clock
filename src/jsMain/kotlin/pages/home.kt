b21ac74b9b5b5270f298e9151c95a2904aa3ade3
Kydesss <90478946+Kydesss@users.noreply.github.com>
2023-03-22 17:52:50 -0400

    Abstracted Bootstrap classes

--- src/jsMain/kotlin/pages/home.kt
+++ src/jsMain/kotlin/pages/home.kt
@@ -11,6 +11,8 @@ import react.dom.html.ReactHTML.img
 import react.dom.html.ReactHTML.p
 
 val homePage = FC<Props> {
+    div {
+        className = ClassName("background")
 //    div {
 //        className = ClassName("flex-row")
 //        div {
@@ -66,53 +68,54 @@ val homePage = FC<Props> {
 //            }
 //        }
 //    }
-    div {
-        className = ClassName("container")
         div {
-            className = ClassName("row")
+            className = ClassName("container")
             div {
-                className = ClassName("col-12 col-lg-6")
-                h1 {
-                    className = ClassName("page-header")
-                    +("CloudClock")
-                }
-                p {
-                    +("Welcome to ")
-                    b {
+                className = ClassName("row")
+                div {
+                    className = ClassName("col-12 col-lg-6")
+                    h1 {
                         className = ClassName("page-header")
                         +("CloudClock")
                     }
-                    +(", the web-based application for time tracking and employee management!")
-                }
-                p {
-                    +("Easily track employee hours. Manage employees from the employee list." +
-                            " Set business hours and cross-check them with employee hours. " +
-                            "Export time sheets for selected employees.")
-                }
-                p {
-                    +("Choose ")
-                    b {
-                        className = ClassName("page-header")
-                        +("CloudClock")
+                    p {
+                        +("Welcome to ")
+                        b {
+                            className = ClassName("page-header")
+                            +("CloudClock")
+                        }
+                        +(", the web-based application for time tracking and employee management!")
                     }
-                    +("for the ultimate time tracking and employee management solution for your" +
-                            "business.")
+                    p {
+                        +("Easily track employee hours. Manage employees from the employee list." +
+                                " Set business hours and cross-check them with employee hours. " +
+                                "Export time sheets for selected employees.")
+                    }
+                    p {
+                        +("Choose ")
+                        b {
+                            className = ClassName("page-header")
+                            +("CloudClock")
+                        }
+                        +("for the ultimate time tracking and employee management solution for your" +
+                                "business.")
+                    }
+                    div {
+                        className = ClassName("btn")
+                        id = "learn-more-btn"
+                        h3 {
+                            id = "learn-more"
+                            +("Learn More")
+                        }
+                    }
                 }
                 div {
-                    className = ClassName("btn")
-                    id = "learn-more-btn"
-                    h3 {
-                        id = "learn-more"
-                        +("Learn More")
+                    className = ClassName("col-12 col-lg-6")
+                    img {
+                        className = ClassName("img-fluid")
+                        src = "/static/img/clock.png"
+                        alt = "A clock."
                     }
-                }
-            }
-            div {
-                className = ClassName("col-12 col-lg-6")
-                img {
-                    className = ClassName("img-fluid")
-                    src = "/static/img/clock.png"
-                    alt = "A clock."
                 }
             }
         }
