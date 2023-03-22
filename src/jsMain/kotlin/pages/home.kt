dc76f38cb94d09fd4119f4d17c8b199e10714cbb
Kydesss <90478946+Kydesss@users.noreply.github.com>
2023-03-22 16:46:12 -0400

    Added more style + button to html

--- src/jsMain/kotlin/pages/home.kt
+++ src/jsMain/kotlin/pages/home.kt
@@ -6,10 +6,66 @@ import react.Props
 import react.dom.html.ReactHTML.b
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.h1
+import react.dom.html.ReactHTML.h3
 import react.dom.html.ReactHTML.img
 import react.dom.html.ReactHTML.p
 
 val homePage = FC<Props> {
+//    div {
+//        className = ClassName("flex-row")
+//        div {
+//            id = "logo"
+//            className = ClassName("flex-md-column")
+////            src = index.html
+//            img {
+//                className = ClassName("img-fluid")
+//                src = "img/logo.png"
+//                alt = "ClockCloud Logo"
+//            }
+//            b {
+//                className = ClassName("logo")
+//                +("CloudClock")
+//            }
+//        }
+//        div {
+//            className = ClassName("flex-md-column")
+//            h3 {
+//                +("About Us")
+//            }
+//        }
+//        div {
+//            className = ClassName("flex-md-column")
+//            h3 {
+//                +("Contact")
+//            }
+//        }
+//        div {
+//            className = ClassName("flex-md-column")
+//            h3 {
+//                +("Pricing")
+//            }
+//        }
+//        div {
+//            className = ClassName("flex-md-column")
+//            h3 {
+//                +("How It Works")
+//            }
+//        }
+//        div {
+//            className = ClassName("col")
+//        }
+//        div {
+//            className = ClassName("flex-md-column")
+//            div {
+//                className = ClassName("btn")
+//                id = "login"
+//                h3 {
+//                    id = "login-text"
+//                    +("Login")
+//                }
+//            }
+//        }
+//    }
     div {
         className = ClassName("container")
         div {
@@ -42,6 +98,14 @@ val homePage = FC<Props> {
                     +("for the ultimate time tracking and employee management solution for your" +
                             "business.")
                 }
+                div {
+                    className = ClassName("btn")
+                    id = "learn-more-btn"
+                    h3 {
+                        id = "learn-more"
+                        +("Learn More")
+                    }
+                }
             }
             div {
                 className = ClassName("col-12 col-lg-6")
@@ -54,3 +118,4 @@ val homePage = FC<Props> {
         }
     }
 }
+
